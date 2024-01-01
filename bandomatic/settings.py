7b71bb2e8dcb2c7f1718310aba27684464af6c8a
mesolimbo <mesolimbo@users.noreply.github.com>
2024-01-01 13:20:25 -0500

    Models for band name randomizer

--- bandomatic/settings.py
+++ bandomatic/settings.py
@@ -70,7 +70,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'greet'
+    'greet',
+    'randomizer'
 ]
 
 MIDDLEWARE = [
--- randomizer/models.py
+++ randomizer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Word(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.OneToOneField(Category, on_delete=models.CASCADE)
