2c368e0ffa7ebeb3ba1b83047cb9b603fc1973ea
mesolimbo <mesolimbo@users.noreply.github.com>
2024-01-05 03:36:22 -0500

    Sample Band artwork gallery from the Band Name GPT

--- bandomatic/urls.py
+++ bandomatic/urls.py
@@ -12,15 +12,17 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')
 """
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 import greet.views
 
 urlpatterns = [
+    path('gallery.html', TemplateView.as_view(template_name="gallery.html"), name='band_gallery'),
     path('privacy.html', greet.views.privacy, name='privacy'),
     path('api/v1/', include("restapi.urls"), name='api-docs'),
     # Uncomment this and the entry in `INSTALLED_APPS` if you wish to use the Django admin feature:
